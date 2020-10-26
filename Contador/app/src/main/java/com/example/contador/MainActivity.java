471cfa616d3b2a78e3335f67b6fd135667ae7448
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-10-25 20:53:48 -0600

    Añadiendo EditText resetar a valor establecido y CheckBox conteo negativo

--- Contador/app/src/main/java/com/example/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/contador/MainActivity.java
@@ -3,36 +3,60 @@ package com.example.contador;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     public int contador;
 
+    TextView textoResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textoResultado=(TextView)findViewById(R.id.contadorTexto);
         contador = 0;
     }
 
     public void incrementaContador(View vista){
         contador++;
-        mostrarResultado();
+        textoResultado.setText(""+contador);
+        //mostrarResultado();
     }
 
     public void restaContador(View vista){
         contador--;
-        mostrarResultado();
+
+        if(contador<0) {
+            CheckBox negativos = (CheckBox)findViewById(R.id.negativos);
+            if(!negativos.isChecked()){
+                contador=0;
+            }
+        }
+
+        textoResultado.setText(""+contador);
+        //mostrarResultado();
     }
 
     public void reseteaContador(View vista){
-        contador=0;
-        mostrarResultado();
+        EditText numero_reset = (EditText)findViewById(R.id.n_reseteo);
+
+        try {
+            contador = Integer.parseInt(numero_reset.getText().toString());
+        } catch(Exception e){
+            contador=0;
+        }
+
+        numero_reset.setText("");
+        textoResultado.setText(""+contador);
+        //mostrarResultado();
     }
 
-    public void mostrarResultado(){
+    /*public void mostrarResultado(){
         TextView textoResultado = (TextView)findViewById(R.id.contadorTexto);
         textoResultado.setText("" + contador);
-    }
+    }*/
 }

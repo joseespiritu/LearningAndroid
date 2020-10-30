374e3a2501246255e26485454088c8f1542b383c
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-10-30 10:40:36 -0600

    Acceder a recursos (Imagen) desde XML y class Java

--- PrimeraApp/app/src/main/java/com/example/primeraapp/MainActivity.java
+++ PrimeraApp/app/src/main/java/com/example/primeraapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.primeraapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView img = (ImageView)findViewById(R.id.ImagenRoca);
+        img.setImageResource(R.drawable.roca_hr);
     }
 }

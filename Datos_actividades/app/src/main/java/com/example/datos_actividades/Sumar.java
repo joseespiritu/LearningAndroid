bff97ffad9f0c3328abe426b0a94dbbea3581458
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-10-31 12:03:31 -0600

    Vista App datos activity

---
+++ Datos_actividades/app/src/main/java/com/example/datos_actividades/Sumar.java
@@ -0,0 +1,12 @@
+package com.example.datos_actividades;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Sumar extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resultado);
+    }
+}

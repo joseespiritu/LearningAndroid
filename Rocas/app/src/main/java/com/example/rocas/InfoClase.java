cb16e943e5418e24885fb3807732cd159523d1e7
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-10-31 11:43:22 -0600

    Creacion de activity, llamando evento desde boton

---
+++ Rocas/app/src/main/java/com/example/rocas/InfoClase.java
@@ -0,0 +1,14 @@
+package com.example.rocas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InfoClase extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+
+}

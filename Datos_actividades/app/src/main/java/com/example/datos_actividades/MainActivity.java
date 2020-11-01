13a00f6ad23f100aaf9ac842a7569936d27b764d
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-11-01 17:18:14 -0600

    Pasar datos de una actividad a otra, atraves de putExtra, getExtras

--- Datos_actividades/app/src/main/java/com/example/datos_actividades/MainActivity.java
+++ Datos_actividades/app/src/main/java/com/example/datos_actividades/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void aSumar(View view){
+
+        EditText cuadro1 = (EditText)findViewById(R.id.num1);
+        EditText cuadro2 = (EditText)findViewById(R.id.num2);
+
+        int n1 = Integer.parseInt(cuadro1.getText().toString());
+        int n2 = Integer.parseInt(cuadro2.getText().toString());
+
         Intent i = new Intent(this,Sumar.class);
+
+        //Metodo para llevar datos a vista
+        i.putExtra("numero1",n1);
+        i.putExtra("numero2",n2);
+
         startActivity(i);
     }
 }
--- Datos_actividades/app/src/main/java/com/example/datos_actividades/Sumar.java
+++ Datos_actividades/app/src/main/java/com/example/datos_actividades/Sumar.java
@@ -2,11 +2,24 @@ package com.example.datos_actividades;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Sumar extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resultado);
+
+        Bundle datos = getIntent().getExtras();
+
+        int num1 = datos.getInt("numero1");
+
+        int num2 = datos.getInt("numero2");
+
+        int resultado = num1+num2;
+
+        TextView valor_resultado = (TextView)findViewById(R.id.texto_resultado);
+
+        valor_resultado.setText("El resultado es: " + resultado);
     }
 }

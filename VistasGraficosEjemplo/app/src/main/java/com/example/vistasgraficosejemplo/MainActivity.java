ff0b0013951fa0d5f84ec17ca93fe03d0003817c
joseespiritu <joseespiritu89@aragon.unam.mx>
2020-11-03 21:02:09 -0600

    Clase Path, Trazos

--- VistasGraficosEjemplo/app/src/main/java/com/example/vistasgraficosejemplo/MainActivity.java
+++ VistasGraficosEjemplo/app/src/main/java/com/example/vistasgraficosejemplo/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 
@@ -24,12 +26,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         protected void onDraw(Canvas canvas){
-            Paint miPincel = new Paint();
+            /*Paint miPincel = new Paint();
             miPincel.setColor(0xff00ffff);
             //miPincel.setColor(Color.argb(185,168,155,255));
             miPincel.setStrokeWidth(8);
             miPincel.setStyle(Paint.Style.STROKE);
-
             canvas.drawCircle(175,175,100, miPincel);
 
             //Relleno
@@ -37,7 +38,20 @@ public class MainActivity extends AppCompatActivity {
             miColor = ContextCompat.getColor(getContext(),R.color.rellenoCirculo);
             miPincel.setColor(miColor);
             miPincel.setStyle(Paint.Style.FILL);
-            canvas.drawCircle(175,175,100, miPincel);
+            canvas.drawCircle(175,175,100, miPincel);*/
+
+            Path miTrazo = new Path();
+            miTrazo.addCircle(350,350,200,Path.Direction.CCW);
+            Paint miPincel = new Paint();
+            miPincel.setColor(Color.RED);
+            miPincel.setStrokeWidth(8);
+            miPincel.setStyle(Paint.Style.STROKE);
+            canvas.drawPath(miTrazo,miPincel);
+            miPincel.setStrokeWidth(1);
+            miPincel.setStyle(Paint.Style.FILL);
+            miPincel.setTextSize(40);
+            miPincel.setTypeface(Typeface.SANS_SERIF);
+            canvas.drawTextOnPath("Curso Android", miTrazo, 40, 100, miPincel);
         }
     }
 }

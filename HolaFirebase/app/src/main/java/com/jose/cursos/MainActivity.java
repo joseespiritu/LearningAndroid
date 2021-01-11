daa6c7edc8135af2556b7b9d07b2142a215bbb40
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-01-11 08:20:02 -0600

    Hola Firebase

--- HolaFirebase/app/src/main/java/com/jose/cursos/MainActivity.java
+++ HolaFirebase/app/src/main/java/com/jose/cursos/MainActivity.java
@@ -1,14 +1,48 @@
 package com.jose.cursos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PATH_START = "start";
+    private static final String PATH_MESSAGE = "message";
+
+    private FirebaseDatabase dataBase;
+    private DatabaseReference mRootreference;
+    private DatabaseReference mChildreference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textViewMessage = findViewById(R.id.textviewMessage);
+
+        dataBase = FirebaseDatabase.getInstance();
+        mRootreference = dataBase.getReference(PATH_START);
+        mChildreference = mRootreference.child(PATH_MESSAGE);
+
+        mChildreference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                textViewMessage.setText(dataSnapshot.getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Error al consultar en firebase", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }

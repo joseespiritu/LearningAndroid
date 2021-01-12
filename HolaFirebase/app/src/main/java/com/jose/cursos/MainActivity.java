3c17c6b7240e718bee8da209cd5bd8a413cc3b39
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-01-11 21:37:36 -0600

    Butterknife y envio de mensajes a RDB

--- HolaFirebase/app/src/main/java/com/jose/cursos/MainActivity.java
+++ HolaFirebase/app/src/main/java/com/jose/cursos/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,10 +15,18 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String PATH_START = "start";
     private static final String PATH_MESSAGE = "message";
+    @BindView(R.id.etMessage)
+    EditText etMessage;
+    @BindView(R.id.btnSend)
+    Button btnSend;
 
     private FirebaseDatabase dataBase;
     private DatabaseReference mRootreference;
@@ -26,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         final TextView textViewMessage = findViewById(R.id.textviewMessage);
 
@@ -44,5 +55,15 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Error al consultar en firebase", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @OnClick(R.id.btnSend)
+    public void onViewClicked(){
+        dataBase = FirebaseDatabase.getInstance();
+        mRootreference = dataBase.getReference(PATH_START);
+        mChildreference = mRootreference.child(PATH_MESSAGE);
+
+        mChildreference.setValue(etMessage.getText().toString().trim());
+        etMessage.setText("");
     }
 }

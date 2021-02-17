88af1915b04cc65713f6e6b0662e9cedab483621
lalofcaunam <lalo.fca.unam@gmail.com>
2021-02-16 20:04:38 -0600

    Example Basic of Listview

---
+++ ListviewDemo/app/src/main/java/com/joseespiritu/listviewdemo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.joseespiritu.listviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView myListView = findViewById(R.id.myListView);
+
+        ArrayList<String> myFamily = new ArrayList<String>();
+
+        myFamily.add("Nick");
+        myFamily.add("Sarah");
+        myFamily.add("John");
+        myFamily.add("Fido");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
+
+        myListView.setAdapter(arrayAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Person selected: ", myFamily.get(position));
+            }
+        });
+    }
+}
